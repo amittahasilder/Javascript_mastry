ad97c482230e726265d236c50c44a1b35553ae48
amittahasilder <amittahasilder@gmail.com>
2026-01-14 10:29:07 -0800

    your message

---
+++ java_basic/dom/dommanipution/index.js
@@ -0,0 +1,17 @@
+
+// const abcd = document.querySelector("#abccd");
+// abcd.innerHTML = "This issssss amit";
+
+
+
+// let h1 = document.querySelector("h1");
+
+// h1.classList.remove("hulu");
+
+// let h2 = document.querySelector("h2");
+// h2.classList.add("hulu");
+
+
+function changeText() {
+  document.getElementById("text").innerText = "Hello JavaScript!";
+}
