7bbd554e386dab5d5510fc9271cc5c86843abbd2
amittahasilder <amittahasilder@gmail.com>
2026-02-03 09:52:46 -0800

    added new file

--- java_basic/function/fun.js
+++ java_basic/function/fun.js
@@ -11,3 +11,18 @@ const greet = function() {
   return "Hi there!";
 };
 console.log(greet());
+
+
+function add(v1, v2){
+    console.log(v1,v2);
+}
+add();
+
+
+function num(n, callback) {
+    return callback(n);
+}
+
+const double = (n) => n * 2;
+
+console.log(num(5, double));
